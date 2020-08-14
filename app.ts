205a27bd319a39faf74a8864d7864da0e4a8c0b9
Francis Ferri <francisferrir@gmail.com>
2020-08-13 21:27:39 -0500

    Tipado de retorno

--- app.ts
+++ app.ts
@@ -1,5 +1,16 @@
 (() => {
-    
+    const sumar = (a: number, b: number): number =>  a + b;
+    const nombre = (): string => "Hola Francis";
+    const obtenerSalario = (): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            resolve("Francis")
+
+        });
+    }
+
+    obtenerSalario()
+        .then(a => console.log(a.toUpperCase))
+
 })();
 
 
