30c4ade147b54d019fb3d334c538e2f07fc7d9b8
Francis Ferri <francisferrir@gmail.com>
2020-08-23 21:12:40 -0500

    Rutas con parametros

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +10,19 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) {
-    console.log("Constructor");
-    
-   }
+  constructor(
+    private _heroesService: HeroesService,
+    private router: Router  
+  ) {}
 
   ngOnInit(): void {
     console.log("ngOnInit");
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
 
+  verHeroe(idx: number){
+    this.router.navigate([ "/heroe", idx]);
   }
 
 }
