6bce6461d03cabac96967eebacd74a085c22287e
Francis Ferri <francisferrir@gmail.com>
2020-09-03 07:59:32 -0500

    Pipe personalizado - contraseña optimizado

--- 03-pipes/src/app/pipes/contrasena.pipe.ts
+++ 03-pipes/src/app/pipes/contrasena.pipe.ts
@@ -5,15 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ContrasenaPipe implements PipeTransform {
 
-  transform(value: string, activado: boolean): string {
-    if (activado){
+  transform(value: string, noMostrar: boolean = false): string {
+    
+    return (noMostrar)? "*".repeat(value.length): value;
+    /* if (activado){
       let password: string = ""
       for (let i = 0 ; i < value.length; i++){
         password += "*"
       }
       return password;
-    }
-    return value;
+    } */
   }
 
 }
