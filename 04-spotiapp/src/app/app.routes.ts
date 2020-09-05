fed9e2f61d349b70eaf8dfeab3b99b2f8cce9b70
Francis Ferri <francisferrir@gmail.com>
2020-09-04 21:25:38 -0500

    Spotiapp: creando rutas

---
+++ 04-spotiapp/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../../../02-spa/src/app/components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+export const ROUTES: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "search", component: SearchComponent},
+    // Cualquier path vacio me redirecciona a home
+    {path: "", pathMatch:"full", redirectTo: "home"},
+    // Cualquier otro path me redirecciona al home
+    {path: "**", pathMatch:"full", redirectTo: "home"}
+]
