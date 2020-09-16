b118a24bbe617fb044bde347a4f371d0a7c130f4
Francis Ferri <francisferrir@gmail.com>
2020-09-16 18:31:05 -0500

    Operador Map

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,14 +15,14 @@ export class SpotifyService {
   getNewReleases(){
     const headers = new HttpHeaders({
       Authorization: "Bearer BQDQN-RMvv5deN6-lzsUMJB2V4Zjb5HByu-8Mw8hQJrehW1q7pYGw8fgyQ-92lW90XBUA_SWPAX7SKxVRsI"
-    })
-      return this.http.get("https://api.spotify.com/v1/browse/new-releases", {headers});
+    });
+      return this.http.get("https://api.spotify.com/v1/browse/new-releases", {headers}).pipe(map( data => data["albums"].items))
   }
 
   getArtista( termino: string){
     const headers = new HttpHeaders({
       Authorization: "Bearer BQDQN-RMvv5deN6-lzsUMJB2V4Zjb5HByu-8Mw8hQJrehW1q7pYGw8fgyQ-92lW90XBUA_SWPAX7SKxVRsI"
     })
-      return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+      return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers}).pipe(map( data => data["artists"].items))
   }
 }
