a1a869ad7f2f3799595fd821bab4dbd4a1ae5b3c
Francis Ferri <francisferrir@gmail.com>
2020-09-25 08:34:05 -0500

    Agregar una lista

--- 05-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,12 +12,45 @@ export class Tab1Page {
   
   constructor(
     public deseosService: DeseosService,
-    private router: Router
+    private router: Router,
+    private alertController: AlertController
   ) { }
 
-  agregarLista(){
-    const url = ["/tabs", "tab1", "agregar"]
-    this.router.navigate(url);
+  async agregarLista(){
+    /* const url = ["/tabs", "tab1", "agregar"]
+    this.router.navigate(url); */
+
+    const alert = await this.alertController.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: "titulo",
+          type: "text",
+          placeholder: "Nomvre de la lista"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancelar");
+          }
+        },
+        {
+          text: "Crear",
+          handler: (data) => {
+            console.log(data);
+            if(data.titulo.length === 0){
+              return;
+            }
+            this.deseosService.crearLista(data.titulo);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -13,4 +13,10 @@ export class DeseosService {
     const lista2 = new Lista("heroes a desaparecer");
     this.listas.push(lista1, lista2);
   }
+
+
+  crearLista(titulo: string){
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+  }
 }
