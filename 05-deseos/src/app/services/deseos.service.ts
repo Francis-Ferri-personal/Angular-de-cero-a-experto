29aa9a5fa27b1c7d585632c291b9b674f5b506cc
Francis Ferri <francisferrir@gmail.com>
2020-09-25 08:58:24 -0500

    Localstorage

--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -9,14 +9,24 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() { 
-    const lista1 = new Lista("Recolectar piedras del inmnfinitp");
-    const lista2 = new Lista("heroes a desaparecer");
-    this.listas.push(lista1, lista2);
+    this.cargarStorage();
   }
 
 
   crearLista(titulo: string){
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem("data",JSON.stringify(this.listas));
+  }
+
+  cargarStorage(){
+    const listaLS = localStorage.getItem("data");
+    if(listaLS){
+      this.listas = JSON.parse(listaLS);
+    }
   }
 }
