1f6157b168f4704ed25a65414e293e87d1252bc2
Francis Ferri <francisferrir@gmail.com>
2020-10-05 22:17:45 -0500

    Rutas hijas archivo separado

--- 06-miscelaneos/src/app/app-routing.module.ts
+++ 06-miscelaneos/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UserRoutes } from './components/usuario/usuario-routing.module';
 
 
 const routes: Routes = [
@@ -12,12 +10,7 @@ const routes: Routes = [
   {
     path: "usuario/:id", 
     component: UsuarioComponent,
-    children: [
-      {path: "nuevo", component: UsuarioNuevoComponent},
-      {path: "editar", component: UsuarioEditarComponent},
-      {path: "detalle", component: UsuarioDetalleComponent},
-      {path: "**", pathMatch: "full", redirectTo: "nuevo"}
-    ]
+    children: UserRoutes
   },
   {path: "**", pathMatch: "full", redirectTo: "home"}
 ];
